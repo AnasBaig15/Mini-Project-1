c2303fdd9541dd286dca45d0db6d6f0c945c0558
Anas Baig15 <baiganas674@gmail.com>
2025-01-22 02:05:30 -0800

    commit

--- app.js
+++ app.js
@@ -1,118 +1,147 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const userModal = require('./modals/userModal');
-const postModal = require('./modals/post');
-const cookieParser = require('cookie-parser');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const userModal = require("./modals/userModal");
+const postModal = require("./modals/post");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-app.set("view engine", 'ejs');
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.render("index");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/login', (req, res) => {
-    res.render("login");
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
-app.get('/profile', isLoggedIn, async (req, res) => {
-    try {
-        let user = await userModal.findOne({ email: req.user.email }).populate("posts");
-        console.log(user.posts)
-        if (!user) {
-            return res.status(404).send("User not found");
-        }
-
-        res.render("profile", { user });
-    } catch (err) {
-        console.error("Error fetching user:", err);
-        res.status(500).send("An error occurred while fetching user data");
+app.get("/profile", isLoggedIn, async (req, res) => {
+  try {
+    let user = await userModal
+      .findOne({ email: req.user.email })
+      .populate("posts");
+    console.log(user.posts);
+    if (!user) {
+      return res.status(404).send("User not found");
     }
+
+    res.render("profile", { user });
+  } catch (err) {
+    console.error("Error fetching user:", err);
+    res.status(500).send("An error occurred while fetching user data");
+  }
 });
-app.post('/post', isLoggedIn, async (req, res) => {
-    try {
-        let user = await userModal.findOne({ email: req.user.email });
-        let content = req.body.content; // Correctly retrieve the content
-        if (!content) return res.status(400).send("Content is required");
-
-        let post = await postModal.create({
-            user: user._id,
-            content
-        });
-
-        user.posts.push(post._id);
-        await user.save();
-
-        res.redirect("/profile");
-    } catch (err) {
-        console.error("Error creating post:", err);
-        res.status(500).send("An error occurred while creating the post");
+app.get("/like/:id", isLoggedIn, async (req, res) => {
+  try {
+    let post = await postModal.findOne({ _id: req.params.id }).populate("user");
+    if (!post) {
+      return res.status(404).send("Post not found");
     }
+
+    const likeIndex = post.likes.indexOf(req.user.userId);
+    if (likeIndex === -1) {
+      post.likes.push(req.user.userId);
+    } else {
+      post.likes.splice(likeIndex, 1);
+    }
+
+    await post.save();
+
+    res.redirect("/profile");
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("An error occurred while processing the like.");
+  }
 });
+app.get('/edit/:id', isLoggedIn, async (req, res)=>{
+    let post = await postModal.findOne({_id: req.params.id}).populate("user");
+    res.render('edit', {post})
+})
+app.post('/update/:id', isLoggedIn, async (req, res)=>{
+    let post = await postModal.findOneAndUpdate({_id: req.params.id}, {content: req.body.content})
+    res.redirect('/profile');
+})
 
+app.post("/post", isLoggedIn, async (req, res) => {
+  try {
+    let user = await userModal.findOne({ email: req.user.email });
+    let content = req.body.content;
+    if (!content) return res.status(400).send("Content is required");
 
-
-app.post('/register', async (req, res) => {
-    let { email, password, username, name, age } = req.body;
-    let user = await userModal.findOne({ email });
-    if (user) return res.status(500).send("User already registered");
-
-    bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(password, salt, async (err, hash) => {
-            let user = await userModal.create({
-                username,
-                email,
-                age,
-                name,
-                password: hash
-            });
-            let token = jwt.sign({ email: email, userId: user._id }, "shhhh");
-            res.cookie("token", token);
-            res.send("Registered");
-        });
+    let post = await postModal.create({
+      user: user._id,
+      content,
     });
+
+    user.posts.push(post._id);
+    await user.save();
+
+    res.redirect("/profile");
+  } catch (err) {
+    console.error("Error creating post:", err);
+    res.status(500).send("An error occurred while creating the post");
+  }
 });
 
-app.post('/login', async (req, res) => {
-    let { email, password } = req.body;
-    let user = await userModal.findOne({ email });
-    if (!user) return res.status(500).send("User not found");
+app.post("/register", async (req, res) => {
+  let { email, password, username, name, age } = req.body;
+  let user = await userModal.findOne({ email });
+  if (user) return res.status(500).send("User already registered");
 
-    bcrypt.compare(password, user.password, (err, result) => {
-        if (result) {
-            let token = jwt.sign({ email: email, userId: user._id }, "shhhh");
-            res.cookie("token", token);
-            res.status(200).redirect("/profile");
-        } else {
-            res.redirect('/login');
-        }
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      let user = await userModal.create({
+        username,
+        email,
+        age,
+        name,
+        password: hash,
+      });
+      let token = jwt.sign({ email: email, userId: user._id }, "shhhh");
+      res.cookie("token", token);
+      res.send("Registered");
     });
+  });
 });
 
-app.get('/logout', (req, res) => {
-    res.cookie("token", "", { maxAge: 0 });
-    res.redirect("/login");
+app.post("/login", async (req, res) => {
+  let { email, password } = req.body;
+  let user = await userModal.findOne({ email });
+  if (!user) return res.status(500).send("User not found");
+
+  bcrypt.compare(password, user.password, (err, result) => {
+    if (result) {
+      let token = jwt.sign({ email: email, userId: user._id }, "shhhh");
+      res.cookie("token", token);
+      res.status(200).redirect("/profile");
+    } else {
+      res.redirect("/login");
+    }
+  });
 });
 
-// Middleware
+app.get("/logout", (req, res) => {
+  res.cookie("token", "", { maxAge: 0 });
+  res.redirect("/login");
+});
+
 function isLoggedIn(req, res, next) {
-    if (!req.cookies.token) {
-        return res.redirect("/login");
-    }
+  if (!req.cookies.token) {
+    return res.redirect("/login");
+  }
 
-    try {
-        let data = jwt.verify(req.cookies.token, "shhhh");
-        req.user = data;
-        next();
-    } catch (err) {
-        res.cookie("token", "", { maxAge: 0 });
-        return res.redirect("/login");
-    }
+  try {
+    let data = jwt.verify(req.cookies.token, "shhhh");
+    req.user = data;
+    next();
+  } catch (err) {
+    res.cookie("token", "", { maxAge: 0 });
+    return res.redirect("/login");
+  }
 }
 
-// Start the server
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
